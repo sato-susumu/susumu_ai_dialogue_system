a0bf157484813bbe214938e19fefa316133f2616
Sato Susumu <75652942+sato-susumu@users.noreply.github.com>
2023-03-29 23:18:30 +0900

    アプリ起動時に足りないフォルダがあれば作成する機能を追加

--- main.py
+++ main.py
@@ -5,7 +5,23 @@ import PySimpleGUI as Sg
 from susumu_toolbox.infrastructure.config import Config
 from susumu_toolbox.ui.main_window import MainWindow
 
+
+def create_model_data_dir_if_needed():
+    model_data_dir = "./model_data"
+    if not os.path.exists(model_data_dir):
+        os.mkdir(model_data_dir)
+
+
+def create_log_dir_if_needed():
+    model_data_dir = "./log"
+    if not os.path.exists(model_data_dir):
+        os.mkdir(model_data_dir)
+
+
 if __name__ == "__main__":
+    create_model_data_dir_if_needed()
+    create_log_dir_if_needed()
+
     # TODO:ログ出力対応
     # TODO:GUIに合わせてドキュメントも更新
     _config = Config()
