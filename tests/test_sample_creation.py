1f0b089558912fec974c8027b9aa8635b1a5e755
Sato Susumu <75652942+sato-susumu@users.noreply.github.com>
2023-03-31 18:56:53 +0900

    テスト追加

--- tests/test_sample_creation.py
+++
@@ -1,9 +0,0 @@
-from susumu_toolbox.application.voice_chat_sample import VoiceChatSample
-from susumu_toolbox.infrastructure.config import Config
-from susumu_toolbox.infrastructure.system_setting import SystemSettings
-
-
-def test_sample_creation_1():
-    config = Config()
-    system_settings = SystemSettings(config)
-    VoiceChatSample(config, system_settings)
