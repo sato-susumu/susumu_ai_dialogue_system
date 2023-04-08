33b7001b813a8b0910525aae0b5fab33c1008280
Sato Susumu <75652942+sato-susumu@users.noreply.github.com>
2023-04-08 13:43:35 +0900

    ビルド関連から不要なモジュールを削除

--- build_exe/build_spec.bat
+++ build_exe/build_spec.bat
@@ -1,4 +1,4 @@
-pyi-makespec main.py --name susumu_ai_dialogue_system  --copy-metadata tqdm --copy-metadata regex --copy-metadata requests --copy-metadata packaging --copy-metadata filelock  --copy-metadata numpy --copy-metadata tokenizers --collect-data ipadic --hidden-import ipadic --collect-all torch
+pyi-makespec main.py --name susumu_ai_dialogue_system  --copy-metadata tqdm --copy-metadata regex --copy-metadata requests --copy-metadata packaging  --hidden-import=tkinter
 echo import sys ; sys.setrecursionlimit(sys.getrecursionlimit() * 5) > temp.txt
 type susumu_ai_dialogue_system.spec >> temp.txt
 move /Y temp.txt susumu_ai_dialogue_system.spec
